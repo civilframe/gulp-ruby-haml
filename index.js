e110bb1c922659bc5ac53c268666d2d237e549c1
Alex Solo <alex.n.solo@gmail.com>
2015-01-06 14:13:33 -0600

    better error handling

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var PluginError = gutil.PluginError;
 var clone = require('clone');
 var map = require('map-stream');
 
-const PLUGIN_NAME = 'gulp-rkkn-ruby-haml';
+const PLUGIN_NAME = 'gulp-ruby-haml-rkkn';
 
 module.exports = function (opt) {
   function modifyFile(file, callback) {
@@ -14,9 +14,7 @@ module.exports = function (opt) {
     }
 
     if (file.isStream()) {
-      this.emit('error',
-                new PluginError(PLUGIN_NAME, 'Streaming not supported'));
-      return callback(null, file);
+      return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'), file);
     }
 
     opt = opt || {};
@@ -43,10 +41,10 @@ module.exports = function (opt) {
     args.push(file.path);
     var cp = spawn(args.shift(), args);
 
-    var self = this;
+    var errorPrepend = file.path + '\n\n';
+
     cp.on('error', function (err) {
-      self.emit('error', new PluginError(PLUGIN_NAME, err));
-      return callback(null, file);
+      return callback(new PluginError(PLUGIN_NAME, errorPrepend + err), file);
     });
 
     var haml_data = '';
@@ -58,14 +56,11 @@ module.exports = function (opt) {
 
     cp.on('close', function (code) {
       if (errors) {
-        self.emit('error', new PluginError(PLUGIN_NAME, errors));
-        return callback(null, null);
+        return callback(new PluginError(PLUGIN_NAME, errorPrepend + errors), file);
       }
 
       if (code > 0) {
-        self.emit('error', new PluginError(PLUGIN_NAME,
-                                           'Exited with error code ' + code));
-        return callback(null, null);
+        return callback(new PluginError(PLUGIN_NAME, 'Exited with error code ' + code), file);
       }
 
       var newFile = clone(file);
