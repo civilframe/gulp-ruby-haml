63fccc9b6cd9687776bb79d6d2230d946092cf2f
Alex Solo <alex.n.solo@gmail.com>
2015-04-29 10:01:41 -0500

    batching ruby workers

--- index.js
+++ index.js
@@ -3,20 +3,35 @@ var gutil = require('gulp-util');
 var Buffer = require('buffer').Buffer;
 var PluginError = gutil.PluginError;
 var clone = require('clone');
-var map = require('map-stream');
+var through = require('through2');
+var q = require('q');
 
 const PLUGIN_NAME = 'gulp-ruby-haml-rkkn';
+const MAX_RUBY_WORKERS = 5;
 
 module.exports = function (opt) {
-  function modifyFile(file, callback) {
+
+  var files = [];
+
+  function bufferFile(file, enc, callback) {
     if (file.isNull()) {
-      return callback(null, file);
+      callback();
+      return;
     }
 
     if (file.isStream()) {
-      return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'), file);
+      this.emit('error', new PluginError(PLUGIN_NAME,  'Streaming not supported'));
+      callback();
+      return;
     }
 
+    files.push(file);
+    callback();
+  };
+
+  function compileFiles(callback) {
+    _this = this;
+    
     opt = opt || {};
     var options = {};
     options.outExtension = opt.outExtension || '.html';
@@ -24,51 +39,90 @@ module.exports = function (opt) {
     options.style = opt.style || null;
     options.noEscapeAttr = opt.noEscapeAttr || false;
     options.requireFile = opt.requireFile || null;
+    options.maxRubyWorkers = opt.maxRubyWorkers || MAX_RUBY_WORKERS;
 
-    var args = ['haml'];
-    if (options.doubleQuote) {
-      args.push('-q');
-    }
-    if (options.style !== null) {
-      args.push('--style', options.style);
-    }
-    if (options.noEscapeAttr === true) {
-      args.push('--no-escape-attrs');
-    }
-    if (options.requireFile !== null) {
-      args.push('--require', options.requireFile);
-    }
-    args.push(file.path);
-    var cp = spawn(args.shift(), args);
-
-    var errorPrepend = file.path + '\n\n';
-
-    cp.on('error', function (err) {
-      return callback(new PluginError(PLUGIN_NAME, errorPrepend + err), file);
-    });
-
-    var haml_data = '';
-    cp.stdout.on('data', function (data) { haml_data += data.toString(); });
-
-    var errors = '';
-    cp.stderr.setEncoding('utf8');
-    cp.stderr.on('data', function (data) { errors += data.toString(); });
-
-    cp.on('close', function (code) {
-      if (errors) {
-        return callback(new PluginError(PLUGIN_NAME, errorPrepend + errors), file);
+    var batches = [];
+    batchCount = 0;
+    batchSize = Math.ceil(files.length / MAX_RUBY_WORKERS)
+    files.forEach(function(file) {
+      if (batchCount <= 0) {
+        batches.push([]);
+        batchCount = batchSize;
       }
 
-      if (code > 0) {
-        return callback(new PluginError(PLUGIN_NAME, 'Exited with error code ' + code), file);
-      }
-
-      var newFile = clone(file);
-      newFile.path = gutil.replaceExtension(file.path, options.outExtension);
-      newFile.contents = new Buffer(haml_data);
-      return callback(null, newFile);
+      batches[batches.length-1].push(file);
+      batchCount--;
     });
-  }
 
-  return map(modifyFile);
+    promises = []
+    batches.forEach(function(batch) {
+      batch.forEach(function(file) {
+
+        promise = q.Promise(function(resolve, reject) {
+          var args = ['haml'];
+          if (options.doubleQuote) {
+            args.push('-q');
+          }
+          if (options.style !== null) {
+            args.push('--style', options.style);
+          }
+          if (options.noEscapeAttr === true) {
+            args.push('--no-escape-attrs');
+          }
+          if (options.requireFile !== null) {
+            args.push('--require', options.requireFile);
+          }
+          args.push(file.path);
+
+          var cp = spawn(args.shift(), args);
+
+          var haml_data = '';
+          cp.stdout.on('data', function (data) { haml_data += data.toString(); });
+
+          var errors = '';
+          cp.stderr.setEncoding('utf8');
+          cp.stderr.on('data', function (data) { errors += data.toString(); });
+
+          var errorPrepend = file.path + '\n\n';
+
+          cp.on('error', function (err) {
+            reject(errorPrepend + err);
+            return;
+          });
+
+          cp.on('close', function (code) {
+            if (errors) {
+              reject(errorPrepend + errors);
+              return;
+            }
+
+            if (code > 0) {
+              reject('Exited with error code ' + code);
+              return;
+            }
+
+            var newFile = clone(file);
+            newFile.path = gutil.replaceExtension(file.path, options.outExtension);
+            newFile.contents = new Buffer(haml_data);
+          
+            _this.push(newFile);
+
+            resolve();
+          });
+        });
+
+        promises.push(promise);
+      });
+    });
+
+    q.all(promises)
+      .fail(function(error) {
+        this.emit('error', new PluginError(PLUGIN_NAME,  error));
+      })
+      .finally(function() {
+        callback();
+      });
+  };
+
+  return through.obj(bufferFile, compileFiles);
 };
